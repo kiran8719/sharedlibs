666f8d88b0417222f461abaa2b4e14167a0be362
kiran8719 <95432395+kiran8719@users.noreply.github.com>
2022-01-16 14:06:02 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -1,9 +1,10 @@
 def call(credId,tomcatIp,tomcatuser){
-  sshagent(['credId']) {
-    // some block
+  sshagent([credId]) {
+  
     sh"""
     scp -o StrictHostKeyChecking=no target/myweb*.war ${tomcatuser}@${tomcatIp}:/opt/tomcat8/webapps/myweb.war
     ssh ${tomcatuser}@${tomcatIp} /opt/tomcat8/bin/shutdown.sh
     ssh ${tomcatuser}@${tomcatIp} /opt/tomcat8/bin/startup.sh
     """
+  }
 }
